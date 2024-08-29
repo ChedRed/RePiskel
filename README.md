34dc64b80df4bf9752630b4138d569fc255c0950
Ryan C <rjchou@icloud.com>
2024-08-28 22:35:46 -0700

    This project is starting to really come together

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Build command (macos):
 
-`g++ main.cpp -I/opt/homebrew/include /opt/homebrew/lib/libSDL3.0.dylib -Wl,-rpath,./ -o repiskel`
+`g++ main.cpp -I/opt/homebrew/include ../../Libraries/Cached/libSDL3.0.dylib -Wl,-rpath,@executable_path/../Resources -o nogit/RePiskel.app/Contents/MacOS/repiskel`
+
 
 (Yea i'll use cmake at some point ¯\_(ツ)_/¯)
--- main.cpp
+++ main.cpp
@@ -2,9 +2,32 @@
 
 #include <SDL3/SDL.h>
 
+SDL_Event e;
+bool loop = true;
+
+struct duo {
+    int x;
+    int y;
+};
+
+int border = 10
+duo leftmargin = { 100.0f, 100.0f, 200.0f, 150.0f };
+SDL_FRect rightmargin = { 100.0f, 100.0f, 200.0f, 150.0f };
+SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Set color to red
+SDL_RenderRect(renderer, &rect);
+
 int main() {
     std::cout << "Initializing SDL3" << std::endl;
-    SDL_Init(SDL_INIT_EVENTS);
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window * window = SDL_CreateWindow("RePiskel",1000,650, SDL_WINDOW_RESIZABLE | SDL_WINDOW_MOUSE_CAPTURE);
+    SDL_Renderer * renderer = SDL_CreateRenderer(window,NULL);
+    SDL_SetWindowMinimumSize(window,800,500);
+    std::cout << "Success! Initializing loop" << std::endl;
+    while (loop) {
+        while (SDL_PollEvent(&e) != 0){
+            if (e.type == 256) loop = false;
+        }
+        SDL_RenderPresent(renderer);
+    }
 
-    return 0;
-}
+    return 0; }
