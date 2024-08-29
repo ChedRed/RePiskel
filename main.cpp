b6720329e2823028a4553a525b5a6bee1dbdf283
ChedRed <rjchou@icloud.com>
2024-08-29 13:37:40 -0700

    Works?

--- main.cpp
+++ main.cpp
@@ -10,11 +10,9 @@ struct duo {
     int y;
 };
 
-int border = 10
-duo leftmargin = { 100.0f, 100.0f, 200.0f, 150.0f };
-SDL_FRect rightmargin = { 100.0f, 100.0f, 200.0f, 150.0f };
-SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Set color to red
-SDL_RenderRect(renderer, &rect);
+int border = 10;
+SDL_FRect leftmargin = { 0,0,100,0 };
+SDL_FRect rightmargin = { 0,0,100,0 };
 
 int main() {
     std::cout << "Initializing SDL3" << std::endl;
@@ -25,8 +23,12 @@ int main() {
     std::cout << "Success! Initializing loop" << std::endl;
     while (loop) {
         while (SDL_PollEvent(&e) != 0){
+            if (e.type == SDL_WINDOWEVENT_RESIZED) leftmargin.h=SDL_GetWindowSize(window)->h;
             if (e.type == 256) loop = false;
         }
+        leftmargin.h = window->h;
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Set color to red
+        SDL_RenderRect(renderer, &leftmargin);
         SDL_RenderPresent(renderer);
     }
 
