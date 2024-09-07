53dbd8dd065b79db4f4231816cce304585015503
Ryan C <rjchou@icloud.com>
2024-09-06 22:22:18 -0700

    Just don't change the window size

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+#include "SDL3/SDL_blendmode.h"
 #include "SDL3/SDL_events.h"
 #include "SDL3/SDL_oldnames.h"
+#include "SDL3/SDL_render.h"
+#include "SDL3/SDL_surface.h"
 #include "SDL3/SDL_timer.h"
 #include "SDL3/SDL_video.h"
+#include <cmath>
 #include <cstddef>
 #include <iostream>
 #include <optional>
@@ -42,17 +46,18 @@ SDL_FRect leftmargin = { 0, 0, margin.a, (float)windowsize.y };
 SDL_FRect rightmargin = { windowsize.x-margin.b, 0, margin.b, (float)windowsize.y };
 SDL_FRect bottomargin = { 0, 36, (float)windowsize.x, 8 };
 SDL_FRect topmargin = { 0, (float)windowsize.y-8, (float)windowsize.x, 8 };
+float olderzoom = 1;
 float oldzoom = 1;
 float zoom = 1;
 
 
 /* Canvas */
-vec2 resolution = { 16, 16 };
+fvec2 resolution = { 0, 0 };
 fvec2 resratio = { (resolution.x<resolution.y)?(float)resolution.x/resolution.y:1, (resolution.x>resolution.y)?(float)resolution.y/resolution.x:1 };
 fvec2 canvascenter = { (margin.a/2)+((windowsize.x-margin.b)/2), (22)+(((float)windowsize.y-8)/2) };
 SDL_FRect precanvas = { margin.a, 44, windowsize.x-margin.a-margin.b, (float)windowsize.y-52 };
 fvec2 canvasize = { resratio.x*((resolution.x<resolution.y)?precanvas.h:precanvas.w), resratio.y*((resolution.x>resolution.y)?precanvas.h:precanvas.w) };
-SDL_FRect canvas = { canvascenter.x-(canvasize.x/2), canvascenter.y-(canvasize.y/2), canvasize.x*zoom, canvasize.y*zoom };
+SDL_FRect canvas = { canvascenter.x-(canvasize.x/2), canvascenter.y-(canvasize.y/2), canvasize.x, canvasize.y };
 
 
 /* Diagnostic variables */
@@ -94,6 +99,16 @@ int main() {
     tirect.x = (int)(windowsize.x/2)-(int)(tirect.w/2);
     tirect.y = 18-(int)(tirect.h/2);
     SDL_DestroySurface(pretitle);
+
+
+    /* Load textures */
+    char texturepath[256];
+    snprintf(texturepath, sizeof(texturepath), "%s%s", SDL_GetBasePath(), "../Resources/test.bmp");
+    SDL_Surface * test = SDL_LoadBMP(texturepath);
+    SDL_Texture * testimg = SDL_CreateTextureFromSurface(renderer, test);
+    SDL_SetTextureScaleMode(testimg, SDL_SCALEMODE_NEAREST);
+    SDL_DestroySurface(test);
+    SDL_GetTextureSize(testimg, &resolution.x, &resolution.y);
 
 
     /* Main loop */
@@ -155,20 +170,22 @@ int main() {
                     topmargin.y = windowsize.y-8;
                     canvascenter = (fvec2){ .x=(margin.a/2)+((windowsize.x-margin.b)/2), .y=(22)+(((float)windowsize.y-8)/2) };
                     precanvas = (SDL_FRect){ .x=margin.a, .y=44, .w=windowsize.x-margin.a-margin.b, .h=(float)windowsize.y-52 };
+                    // canvas = (SDL_FRect){ .x=((canvasize.x*zoom)<=precanvas.w)?canvascenter.x-(canvas.w/2):(canvas.x-mouse.x)+mouse.x, .y=((canvasize.y*zoom)<=precanvas.h)?canvascenter.y-(canvas.h/2):(canvas.y-mouse.y)+mouse.y, .w=canvas.w, .h=canvas.h };
+                    canvas = (SDL_FRect){ .x=0, .y=0, .w=canvas.w*(resratio.x*((precanvas.w>precanvas.h)?precanvas.h:precanvas.w)/canvasize.x), .h=canvas.h*(resratio.y*((precanvas.w>precanvas.h)?precanvas.h:precanvas.w)/canvasize.y) };
                     canvasize = (fvec2){ .x=resratio.x*((precanvas.w>precanvas.h)?precanvas.h:precanvas.w), .y=resratio.y*((precanvas.w>precanvas.h)?precanvas.h:precanvas.w) };
-                    canvas = (SDL_FRect){ .x=(zoom<=1)?canvascenter.x-((canvasize.x*zoom)/2):(float)((canvasize.x>precanvas.w)?:0), .y=(zoom<=1)?canvascenter.y-((canvasize.y*zoom)/2):0, .w=canvasize.x*zoom, .h=canvasize.y*zoom};
+
+
+                /* Zoom canvas */
                 case SDL_EVENT_MOUSE_WHEEL:
                     scroll.x = e.wheel.x;
                     scroll.y = e.wheel.y;
                     if (!(mouse.x >= precanvas.x && mouse.x <= precanvas.x+precanvas.w && mouse.y >= precanvas.y && mouse.y <= precanvas.y + precanvas.h)) break;
-                    zoom = (float)((int)(((zoom+(scroll.y/100)>.1)?zoom+(scroll.y/100):.1)*1000))/1000;
+                    zoom = (zoom+(scroll.y/100)>.1)?((true)?zoom+(scroll.y/100):oldzoom):.1;
                     precanvas = (SDL_FRect){ .x=margin.a, .y=44, .w=windowsize.x-margin.a-margin.b, .h=(float)windowsize.y-52 };
-                    canvasize = (fvec2){ .x=resratio.x*((precanvas.w>precanvas.h)?precanvas.h:precanvas.w), .y=resratio.y*((precanvas.w>precanvas.h)?precanvas.h:precanvas.w) };
-                    canvas = (SDL_FRect){ .x=canvas.x-mouse.x, .y=canvas.y-mouse.y, .w=canvas.w+(canvas.w*(oldzoom-zoom)), .h=canvas.h+(canvas.h*(oldzoom-zoom))};
-                    canvas.x *= zoom;
-                    canvas.y *= zoom;
-                    canvas.x += mouse.x;
-                    canvas.y += mouse.y;
+                    canvas = (SDL_FRect){ .x=(canvas.w<precanvas.w)?canvascenter.x-((canvas.w*(1+zoom-oldzoom))/2):((canvas.x-mouse.x)*(1+zoom-oldzoom))+mouse.x, .y=(canvas.h<precanvas.h)?canvascenter.y-((canvas.h*(1+zoom-oldzoom))/2):((canvas.y-mouse.y)*(1+zoom-oldzoom))+mouse.y, .w=canvas.w*(1+zoom-oldzoom), .h=canvas.h*(1+zoom-oldzoom) };
+
+
+                    /* Guard rails for canvas zoom */
                     if (canvas.w>precanvas.w) {
                         (canvas.x>precanvas.x)?canvas.x=precanvas.x:canvas.x;
                         (canvas.x+canvas.w<precanvas.x+precanvas.w)?canvas.x=precanvas.x-(canvas.w-precanvas.w):canvas.x;
@@ -178,6 +195,9 @@ int main() {
                         (canvas.y+canvas.h<precanvas.y+precanvas.h)?canvas.y=precanvas.y-(canvas.h-precanvas.h):canvas.y;
                     }
                     oldzoom = zoom;
+
+
+                /* Reduce FPS if unfocussed */
                 case SDL_EVENT_WINDOW_FOCUS_GAINED:
                     focus = true;
                     break;
@@ -186,6 +206,13 @@ int main() {
                     break;
             }
         }
+
+
+        /* Render canvas */
+        SDL_SetRenderDrawColor(renderer, 160, 160, 160, 255);
+        // SDL_RenderFillRect(renderer, &upleftbar);
+        // SDL_RenderFillRect(renderer, &downrightbar);
+        SDL_RenderTexture(renderer, testimg, NULL, &canvas);
 
 
         /* Render margins */
@@ -200,15 +227,6 @@ int main() {
         SDL_SetRenderDrawColor(renderer, 29, 29, 29, 255);
         SDL_RenderFillRect(renderer, &bottomargin);
         SDL_RenderFillRect(renderer, &topmargin);
-
-
-        /* Render canvas */
-        SDL_SetRenderDrawColor(renderer, 160, 160, 160, 255);
-        // SDL_RenderFillRect(renderer, &upleftbar);
-        // SDL_RenderFillRect(renderer, &downrightbar);
-        SDL_RenderRect(renderer, &precanvas);
-        SDL_SetRenderDrawColor(renderer, 160, 0, 0, 255);
-        SDL_RenderRect(renderer, &canvas);
 
 
         /* Render title text */
